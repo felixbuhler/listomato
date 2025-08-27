bb234c75b98b3886f006981ddd4d63d3c95d51bc
felixbuhler <felix.buhler@me.com>
2025-08-27 11:32:42 +0200

    Smaller Size

--- src/components/canvas/sketch.js
+++ src/components/canvas/sketch.js
@@ -5,31 +5,35 @@ export const playlistCover = (p) => {
     let canvasSize = window.getComputedStyle(document.documentElement).getPropertyValue('--canvas-size');
     canvasSize = canvasSize.substring(0, canvasSize.length - 2);
 
-
-    console.log(canvasSize)
     // Variables
 
-    let colorWhite, colorBlack, colorTop, colorBottom;
+    let colorWhite, colorBlack, colorTop, colorBottom, font;
+    let baseSize = 100;
+    let minSize = 10;
+
+    window.p = p;
 
     // Noise Generator
 
     let noiseLayer;
 
-    function generateNoise(pg) {
-        pg.loadPixels();
-        for (let i = 0; i < pg.pixels.length; i += 4) {
-            let val = p.random(255);
-            pg.pixels[i] = val;
-            pg.pixels[i + 1] = val;
-            pg.pixels[i + 2] = val;
-            pg.pixels[i + 3] = 15;
+    function generateNoise(pg, blockSize = 1.5) {
+        pg.noStroke();
+        for (let y = 0; y < pg.height; y += blockSize) {
+            for (let x = 0; x < pg.width; x += blockSize) {
+                let val = p.random(255);
+                pg.fill(val, 10); // Grauwert + Alpha
+                pg.rect(x, y, blockSize, blockSize);
+            }
         }
-        pg.updatePixels();
+
     }
 
     // SETUP
 
-    p.setup = () => {
+    p.setup = async () => {
+
+        font = await p.loadFont('fonts/Geist-Medium.ttf');
 
         let canvas = p.createCanvas(canvasSize, canvasSize);
         canvas.parent('playlist-cover');
@@ -37,7 +41,13 @@ export const playlistCover = (p) => {
         // Noise
 
         noiseLayer = p.createGraphics(canvasSize, canvasSize);
+        noiseLayer.pixelDensity(1);
         generateNoise(noiseLayer);
+
+        // Text
+
+        title = p.select("#title");
+
     }
 
     // DRAW
@@ -46,8 +56,9 @@ export const playlistCover = (p) => {
         colorWhite = p.color(240, 240, 240);
         colorBlack = p.color(40, 40, 40)
 
-        colorTop = p.color(240, 120, 20) // document.getElementById("c1").value;
-        colorBottom = p.color(240, 0, 20) // document.getElementById("c2").value;
+        colorTop = document.getElementById("color-top").value;
+        colorBottom = document.getElementById("color-bottom").value;
+
 
         // Rectangles
 
@@ -91,8 +102,64 @@ export const playlistCover = (p) => {
 
         p.image(noiseLayer, 0, 0);
 
+        // Text
 
+        p.push();
+
+        let content = title.value();
+
+        let textSizeValue = baseSize;
+        p.textSize(textSizeValue);
+
+        let width = canvasSize - textSizeValue / 2;
+
+        while (p.textWidth(content) > width * 0.85 && textSizeValue > minSize) {
+            textSizeValue--;
+            p.textSize(textSizeValue);
+        }
+
+        p.fill(colorBlack);
+        p.textFont(font);
+        p.textAlign(p.LEFT, p.TOP);
+        p.textLeading(textSizeValue);
+        p.text(content, textSizeValue / 2, textSizeValue / 3);
+
+        p.pop();
+
+        // Text Dots
+
+        p.push();
+
+        p.fill(colorBlack);
+        p.ellipse(canvasSize - baseSize, canvasSize - baseSize, baseSize * 0.7, baseSize * 0.7)
+
+        p.pop();
+
+        p.push();
+
+        p.stroke(colorBlack);
+        p.strokeWeight(10);
+        p.noFill();
+        p.ellipse(canvasSize - baseSize * 1.8, canvasSize - baseSize, baseSize * 0.7 - 10, baseSize * 0.7 - 10)
+
+        p.pop();
 
     }
 
+    // Export
+
+    function fileexport() {
+        let playlistTitleRaw = document.getElementById("title").value.toUpperCase();
+        let playlistTitle = playlistTitleRaw.replace(/[ \t]+(?=\r?\n)/g, "");
+        playlistTitle = playlistTitle.replace(/\r?\n/g, "_");
+        playlistTitle = playlistTitle.replace(/[ \t]/g, "_");
+
+        p.saveCanvas("LISTOMATO_" + playlistTitle, 'png')
+    }
+
+    const download = document.getElementById("download")
+
+    download.addEventListener("click", function (e) {
+        fileexport();
+    });
 }
